99943c994d8cec45719e492c7814010f930c87d4
ahmadarroziqi <ahmadarroziqi@gmail.com>
2026-01-27 19:08:12 +0700

    update iot

---
+++ libs/submodules/iot/components/Cards/IotInfoCardSection.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+import { StyleSheet } from 'react-native'
+
+interface IotInfoCardSectionProps {
+  sectionText: string
+  action?: ReactNode
+}
+
+function IotInfoCardSection({ sectionText, action }: IotInfoCardSectionProps) {
+  return
+}
+
+export default IotInfoCardSection
+
+const styles = StyleSheet.create({
+  container: {},
+})
