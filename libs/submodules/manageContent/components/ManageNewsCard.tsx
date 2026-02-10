402f3b9ecd09fcbdeab141da4c79913c81ea5dd7
zeronyms <ksdarazi@gmail.com>
2026-02-10 14:41:52 +0700

    change: improve code, separation styling, readibility, refactoring manage content screen

--- libs/submodules/manageContent/components/ManageNewsCard.tsx
+++ libs/submodules/manageContent/components/ManageNewsCard.tsx
@@ -2,7 +2,8 @@ import Flex from '@/components/Flex'
 import { NewsData } from '@/libs/dummyData/adminNewsItem.dummy'
 import { MaterialIcons } from '@expo/vector-icons'
 import { Image } from 'expo-image'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import styles from '../styles/ManageNewsCard.style'
 
 const BLURHASH =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj['
@@ -72,69 +73,3 @@ const ManageNewsCard: React.FC<ManageNewsCardProps> = ({
 }
 
 export default ManageNewsCard
-
-const styles = StyleSheet.create({
-  itemContainer: {
-    padding: 20,
-    borderTopWidth: 1,
-    borderColor: '#E5E7EB',
-  },
-  badgeNumber: {
-    backgroundColor: '#2B9846',
-    width: 26,
-    height: 26,
-    borderRadius: 13,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  badgeText: {
-    color: 'white',
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  itemTitle: {
-    flex: 1,
-    fontSize: 14,
-    fontWeight: '600',
-    color: '#1F2937',
-  },
-  imageContainer: {
-    width: '100%',
-    height: 180,
-    borderRadius: 12,
-    overflow: 'hidden',
-    backgroundColor: '#F3F4F6',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-  },
-  authorText: {
-    fontSize: 14,
-    fontWeight: '600',
-    color: '#374151',
-  },
-  urlText: {
-    fontSize: 12,
-    color: '#032746',
-    textDecorationLine: 'underline',
-  },
-  actionButton: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    height: 44,
-  },
-  editButton: {
-    backgroundColor: '#2B7FFF',
-  },
-  deleteButton: {
-    backgroundColor: '#DC2626',
-  },
-  actionButtonText: {
-    color: 'white',
-    fontSize: 14,
-    fontWeight: '600',
-  },
-})
